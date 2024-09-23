05ac0c7308e55665b0ad491c1caf3b4f05a07b0e
PankovaMS <lmargojayl@gmail.com>
2024-09-23 19:58:17 +0300

    new

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
+//import logo from './logo.svg';
+import huyak from './logo.png';
+import  huyak_2 from './logo_2.png'
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    let savedCount = localStorage.getItem('clickCount') ?? '0';
+
+    // Состояния для счётчика и текущего изображения
+    const [count, setCount] = useState<number>(parseInt(savedCount, 10));
+    const [currentImage, setCurrentImage] = useState<string>(huyak);
+
+    useEffect(() => {
+        if (savedCount) {
+            setCount(parseInt(savedCount, 10));
+        }
+    }, []);
+
+    useEffect(() => {
+        localStorage.setItem('clickCount', String(count));
+    }, [count]);
+
+    // Обработчик нажатия кнопки
+    const handleMouseDown = () => {
+        setCurrentImage(huyak_2); // Меняем изображение, пока кнопка нажата
+    };
+
+    const handleMouseUp = () => {
+        setCount(count + 1);
+        setCurrentImage(huyak); // Возвращаем изображение после отпускания кнопки
+    };
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={currentImage} className="App-logo" alt="logo" />
+                <button
+                    onMouseDown={handleMouseDown}
+                    onMouseUp={handleMouseUp}>
+                    CLICK
+                </button>
+                <p>You clicked {count} times</p>
+            </header>
+        </div>
+    );
 }
 
 export default App;
